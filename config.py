89d4045eb94d773a2b6f249821d4ded19f01f337
Mraprguild8133 <mraprguild06@gmail.com>
2025-10-15 14:50:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 REMOVE_BG_API_KEY = os.getenv('REMOVE_BG_API_KEY')
 
 # Remove.bg API endpoint
-REMOVE_BG_URL = "https://api.remove.bg/v1.0/removebg"        return value
+REMOVE_BG_URL = "https://api.remove.bg/v1.0/removebg"    
 
 # Create global config instance
 try:
